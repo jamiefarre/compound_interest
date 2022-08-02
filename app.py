2756423669bc19f9cd90173fcbb8b4dfcabb1746
jamiehfarre <jamiehfarrell@gmail.com>
2022-08-02 11:44:07 +0100

    change input wording

--- app.py
+++ app.py
@@ -2,12 +2,12 @@
 from calc import monthly_compounding
 
 
-initial = int(input("Input initial value: "))
-monthly = int(input(
-    "Input the value of consistent monthly contributions: "))
+initial = float(input("Please input initial value: "))
+monthly = float(input(
+    "Please input the value of consistent monthly contributions: "))
 annual_rate = float(input(
-    "Input the annual percent floaterest rate, as a decimal between 0 and 1: "))
+    "Please input the annual percent floaterest rate, as a decimal between 0 and 1: "))
 years = int(input(
-    "Input how many years you will be invested for: "))
+    "Please input how many years you will be invested for: "))
 final_sum = monthly_compounding(initial, monthly, years, annual_rate)
 print(final_sum)
